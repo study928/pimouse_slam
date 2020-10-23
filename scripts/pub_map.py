653ba22e517e21aa64b91ac2a488b6250bb4aa71
kuromoto <kuromoto196@kanazawa-es.com>
2020-10-23 17:23:11 +0900

    Add

--- scripts/pub_map.py
+++ scripts/pub_map.py
@@ -7,6 +7,7 @@ from geometry_msgs.msg import Twist, Quaternion, TransformStamped, Point
 from std_srvs.srv import Trigger, TriggerResponse
 from pimouse_ros.srv import TimedMotion
 from nav_msgs.msg import Odometry
+from nav_msgs.msg import OccupancyGrid
 
 class Motor():
     def __init__(self):
@@ -19,7 +20,7 @@ class Motor():
         self.srv_off = rospy.Service('motor_off', Trigger, self.callback_off)
         self.srv_tm = rospy.Service('timed_motion', TimedMotion, self.callback_tm)
 
-        self.pub_odom = rospy.Publisher('odom', Odometry, queue_size=20)
+        self.pub_odom = rospy.Publisher('odom', Odometry, queue_size=10)
         self.bc_odom = tf.TransformBroadcaster()
 
         self.x, self.y, self.th = 0.0, 0.0, 0.0
